a9a3ef7c02f0cd0a01340b1a6418aed3f505a8c4
4KaNE <35267520+4KaNE@users.noreply.github.com>
2018-05-28 04:38:43 +0900

    Ver. 1.0
    ファーストリリース

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # BDOinfo
+黒い砂漠のボスpopを通知するBot  
+ボス湧き時間固定後用
+### 使用方法  
+* [Discord.py](https://github.com/Rapptz/discord.py)をインストール  
+`pip install discord`  
+
+* Botの作成  
+https://discordapp.com/developers/applications/me  
+1 新しいアプリを選択  
+2 アプリケーション名を入力してアプリを作成  
+3 Botユーザーを作成  
+4 Bot欄のトークンをメモして変更を保存  
+5 Generate OAuth2 URLをクリック  
+6 botにチェックが入っているのを確認してURLをCOPYして開く  
+7 Botを自分のサーバーに招待
+
+* 設定の書き換え  
+1 メモしておいたBotのトークンをconfig.iniのBOT_TOKENに上書き  
+2 Discordの設定→テーマ→開発者モードにチェックを入れる  
+3 通知を送るテキストチャンネルを右クリック→IDをコピー  
+4 3でコピーしたチャンネルIDをconfig.iniのCHANNELに上書き  
+5 COMAND_PREFIXとDEL_TIMEの変更はお好みで
+
+* 権限の設定  
+Discordのサーバー設定→役職→Botが属する役職に以下の権限を付与    
+・テキストチャンネルの閲覧・ボイスチャンネルの一覧表示  
+・メッセージを送信  
+・メッセージの管理  
+・埋め込みリンク  
+・リアクションの追加
+
+* 起動  
+bot.pyをダブルクリックで起動、ウインドウを閉じると終了  
+!helpでhelpの表示、!infoでボス時間の表示  
+起動中はBotステータスに次のボスの名前と湧き時間を表示
+
+### 動作環境
+Python 3.5  
+Discord.py 0.6.12
---
+++ bot.py
@@ -0,0 +1,145 @@
+"""This is a main program"""
+import asyncio
+import configparser
+import datetime
+import json
+import re
+import discord
+import time_checker
+
+
+CLIENT = discord.Client()
+INIFILE = configparser.SafeConfigParser()
+INIFILE.read('config.ini', 'UTF-8')
+BOT_TOKEN = INIFILE.get('Discord', 'BOT_TOKEN')
+CHANNEL_ID = INIFILE.get('Discord', 'CHANNEL')
+CHANNEL = discord.Object(id=CHANNEL_ID)
+COMAND_PREFIX = INIFILE.get('Discord', 'COMAND_PREFIX')
+DEL_TIME = int(INIFILE.get('Discord', 'DEL_TIME'))
+POPTIME_JSON = open('poptime.json', 'r', encoding="utf-8_sig")
+JSON_DATA = json.load(POPTIME_JSON)
+CT = time_checker.CheckTime(JSON_DATA)
+
+
+@CLIENT.event
+async def regular_processing():
+    """
+    指定の時間にボスのpopを通知する
+    """
+    while True:
+        now = datetime.datetime.now()
+        time_key, weekday = CT.check_nextpop(now)
+        nowplay = CT.change_presence(time_key, weekday)
+        try:
+            await CLIENT.change_presence(game=discord.Game(name=nowplay))
+        except AttributeError:
+            pass
+
+        res = CT.check_before30(now, time_key, weekday)
+        if res is None:
+            print("...")
+        else:
+            await CLIENT.send_message(CHANNEL, res)
+
+        await asyncio.sleep(60)
+
+
+@CLIENT.event
+async def on_ready():
+    """
+    起動確認
+    """
+    print('Black Desert Online infomation bot')
+    print('Logged in as')
+    print(CLIENT.user.name)
+    print(CLIENT.user.id)
+    CLIENT.loop.create_task(regular_processing())
+
+
+@CLIENT.event
+async def on_message(message):
+    """
+    テキストチャットに反応する
+    """
+    if CLIENT.user != message.author \
+    and message.content.startswith(COMAND_PREFIX):
+
+        if re.search("info", message.content):
+            now = datetime.datetime.now()
+            _, weekday = CT.check_nextpop(now)
+            title, description = CT.info(weekday)
+            res = discord.Embed(title=title,\
+            description=description, colour=0x3498db)
+            msg = await CLIENT.send_message(CHANNEL, embed=res)
+            await CLIENT.add_reaction(msg, '◀')
+            await CLIENT.add_reaction(msg, '▶')
+            await asyncio.sleep(1)
+            CLIENT.loop.create_task(check_reaction(msg, weekday))
+            await asyncio.sleep(DEL_TIME)
+            await CLIENT.delete_message(msg)
+            await CLIENT.delete_message(message)
+
+        elif re.search("help", message.content):
+            res = discord.Embed(title="機能",\
+            description="・ボスの湧き時間10分前、30分前に通知\
+            \n・次のボスの名前と湧き時間をステータスに表示\
+            \n・`{}info`で曜日ごとのボスポップ一覧を表示\
+            \n　(◀で前の曜日、▶で次の曜日)\
+            \n要望、バグ報告は https://github.com/4KaNE/BDOinfo に"\
+            .format(COMAND_PREFIX), colour=0x3498db)
+            msg = await CLIENT.send_message(CHANNEL, embed=res)
+            await asyncio.sleep(DEL_TIME)
+            await CLIENT.delete_message(msg)
+            await CLIENT.delete_message(message)
+
+
+@CLIENT.event
+async def check_reaction(target_msg, weekday):
+    """
+    指定のメッセージにつけられたリアクションを監視する
+    """
+    def weekday_normalize(value):
+        """
+        weekdayの数値を0～6の範囲に直す
+        """
+        if value < 0:
+            value += 7
+
+        elif value > 6:
+            value -= 7
+
+        else:
+            pass
+
+        return value
+
+    endtime = datetime.datetime.now() + datetime.timedelta(seconds=DEL_TIME)
+    while True:
+        if datetime.datetime.now() >= endtime:
+            break
+
+        else:
+            target_reaction = await CLIENT.wait_for_reaction(message=target_msg)
+            if target_reaction.reaction.emoji == '◀':
+                weekday -= 1
+
+            elif target_reaction.reaction.emoji == '▶':
+                weekday += 1
+
+            else:
+                weekday = None
+
+            if weekday is None:
+                pass
+
+            else:
+                weekday = weekday_normalize(weekday)
+                title, description = CT.info(weekday)
+                msg = discord.Embed(title=title,\
+                description=description, colour=0x3498db)
+                await CLIENT.edit_message(target_msg, embed=msg)
+                await CLIENT.remove_reaction(target_msg, \
+                target_reaction.reaction.emoji, target_reaction.user)
+
+
+CLIENT.run(BOT_TOKEN)
